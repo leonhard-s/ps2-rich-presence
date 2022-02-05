cdaa9c3515b361fbf489695ca1c21214d57db4d4
Leonhard S <leonhard-sei@outlook.com>
2022-02-05 13:46:38 +0100

    Create "discord" submodule
    This handles data and functions specific to the Discord application.

---
+++ ps2rpc/discord/_assets.py
@@ -0,0 +1,53 @@
+"""Lists all named assets available in the Discord application.
+
+These keys are used to ensure we only specify known assets in the
+presence data. Be sure to update this list when new assets are added to
+the Discord application.
+"""
+
+__all__ = [
+    'ASSET_KEYS',
+    'is_valid_asset_key',
+]
+
+ASSET_KEYS = [
+    # Infantry classes
+    'infiltrator',
+    'light_assault',
+    'combat_medic',
+    'engineer',
+    'heavy_assult',
+    'max',
+    # Vehicles
+    'ant',
+    'bastion',
+    'chimera',
+    'colossus',
+    'dervish',
+    'engineer',
+    'flash',
+    'galaxy',
+    'harasser',
+    'javelin',
+    'liberator',
+    'lightning',
+    'magrider',
+    'mosquito',
+    'prowler',
+    'reaver',
+    'scythe',
+    'sunderer',
+    'valkyrie',
+    'vanguard',
+    # Zones
+    'amerish',
+    'esamir',
+    'hossin',
+    'indar',
+    'oshur',
+]
+
+
+def is_valid_asset_key(key: str) -> bool:
+    """Return whether the given key is a valid asset key."""
+    return key in ASSET_KEYS
