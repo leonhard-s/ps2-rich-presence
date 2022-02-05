521527dbd84abc5d2c210c7971ecbc6cb71b856c
Leonhard S <leonhard-sei@outlook.com>
2022-02-05 17:36:16 +0100

    Update main Application for new components

--- ps2rpc/_app.py
+++ ps2rpc/_app.py
@@ -5,20 +5,26 @@ from typing import List
 
 from PyQt6.QtWidgets import QApplication
 
-from ._listener import Listener
+from ._rpc import RPCHandler
+from .presence import PresenceFactory
 from ._qasync import connect, slot
-from ._presence import Status, PresenceData
+from .tracking import ActivityTracker
+from .userstate import UserState
 from .ui import MainWindow
 
 __all__ = [
     'Application',
 ]
 
+# Hard-coded character "Auroram" for testing purposes
+_DEBUG_CHAR_ID = 5428072203494645969
+
 
 class Application(QApplication):
     """Main PS2RPC application object."""
 
-    _listener: Listener
+    _listener: ActivityTracker
+    _factory: PresenceFactory
 
     def __init__(self, argv: List[str]) -> None:
         super().__init__(argv)
@@ -27,10 +33,15 @@ class Application(QApplication):
 
     def start_tracker(self, loop: asyncio.AbstractEventLoop) -> None:
         """Start the event tracker."""
-        self._listener = Listener(loop)
-        connect(self._listener.status_changed, self.update_status)
+        self._listener = ActivityTracker(_DEBUG_CHAR_ID, loop)
+        connect(self._listener.state_changed, self.update_status)
 
-    @slot(PresenceData)
-    def update_status(self, status: PresenceData) -> None:
+    @slot()
+    def update_status(self) -> None:
         """Update the status of the client."""
-        Status().update(status)
+        gamestate = self._listener.current_state
+        if not hasattr(self, '_factory'):
+            self._factory = PresenceFactory(gamestate, UserState.CARROTS)
+        # State
+        payload = self._factory.build_presence()
+        RPCHandler().update(payload)
