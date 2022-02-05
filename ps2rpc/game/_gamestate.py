65fa105f2d3ea7c7ab6e4d89b82fa2f43f8fbdd4
Leonhard S <leonhard-sei@outlook.com>
2022-02-05 14:40:29 +0100

    Extend GameStateFactory to handle partial states
    This allows us to hook into event payloads that do not contain all of the information we require to build a full game state (experience payloads may not contain team ID info, etc.).

--- ps2rpc/game/_gamestate.py
+++ ps2rpc/game/_gamestate.py
@@ -28,22 +28,48 @@ class GameStateFactory:
     The class holds references to immutable parts of the game state
     that cannot be inferred from the event service payloads, such as
     the character's ID, faction, or server.
+
+    Additionally, it also keeps track of the recent state of the
+    tracked character so it can be updated incrementally. This is done
+    because not every event service payload contains all the data
+    necessary to produce a valid game state.
     """
 
     def __init__(self, character_id: int, faction: Ps2Faction,
-                 server: Ps2Server) -> None:
+                 server: Ps2Server, initial_class: Ps2Class | None = None
+                 ) -> None:
         self._character_id = character_id
         self._faction = faction
+        self._team = faction
+        self._profile: Ps2Class | Ps2Vehicle = (
+            initial_class or Ps2Class.LIGHT_ASSAULT)
         self._server = server
+        self._zone = Ps2Zone.SANCTUARY
 
-    def build_state(self, team: Ps2Faction, profile: Ps2Class | Ps2Vehicle,
-                    zone: Ps2Zone) -> GameState:
+    @property
+    def character_id(self) -> int:
+        """The character's ID."""
+        return self._character_id
+
+    def build_state(self) -> GameState:
         """Build a `GameState` instance from the given data."""
         return GameState(
             character_id=self._character_id,
             faction=self._faction,
-            team=team,
+            team=self._team,
             server=self._server,
-            profile=profile,
-            zone=zone,
+            profile=self._profile,
+            zone=self._zone,
         )
+
+    def set_profile(self, profile: Ps2Class | Ps2Vehicle) -> None:
+        """Set the profile for the current game state."""
+        self._profile = profile
+
+    def set_team(self, team: Ps2Faction) -> None:
+        """Set the team for the current game state."""
+        self._team = team
+
+    def set_zone(self, zone: Ps2Zone) -> None:
+        """Set the zone for the current game state."""
+        self._zone = zone
