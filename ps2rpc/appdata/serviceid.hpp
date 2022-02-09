175c578c6ed0f8f4b345f61016b4fe682a270e24
Leonhard S <leonhard-sei@outlook.com>
2022-02-09 15:13:59 +0100

    Add service ID define
    Using a define should make it slightly harder for others to grab the service ID - not that I'm particularly worried about it being known considering this runs client-side anyway.

---
+++ ps2rpc/appdata/serviceid.hpp
@@ -0,0 +1,16 @@
+// Copyright 2022 Leonhard S.
+
+#ifndef PS2RPC_APPDATA_SERVICEID_HPP
+#define PS2RPC_APPDATA_SERVICEID_HPP
+
+/**
+ * Used to identify this application to the PlanetSide 2 API. It is tied to
+ * rate limiting and therefore mildly sensitive.
+ *
+ * For details, see <https://census.daybreakgames.com/#service-id>
+ */
+#ifndef SERVICE_ID
+#define SERVICE_ID "s:example"
+#endif // SERVICE_ID
+
+#endif // PS2RPC_APPDATA_SERVICEID_HPP
