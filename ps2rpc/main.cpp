1a053f057043906e5259ce83318e51472996a3a0
Leonhard S <leonhard-sei@outlook.com>
2022-02-11 20:32:21 +0100

    Clear main.cpp

--- ps2rpc/main.cpp
+++ ps2rpc/main.cpp
@@ -1,34 +1,16 @@
 // Copyright 2022 Leonhard S.
 
-#include <QtCore/QDebug>
-#include <QtCore/QTimer>
-#include <QtWidgets/QApplication>
+#include <QApplication>
 
-#include "discord.h"
-#include "arx/ess.hpp"
+#include "config.hpp"
 
-#include "game/state.hpp"
-#include "presence/handler.hpp"
-#include "presence/factory.hpp"
-#include "tracker.hpp"
+using namespace ps2rpc;
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-
-    // Create presence helpers
-    ps2rpc::PresenceHandler presence_handler;
-    ps2rpc::PresenceFactory factory;
-    factory.setActivityIdle();
-
-    // Create activity tracker
-    // TODO: Allow input of tracked character name/ID
-    ps2::CharacterId character_id = 5428072203494645969;
-    ps2rpc::ActivityTracker tracker{character_id};
-
-    // Hook up game state tracking to presence handler
-    QObject::connect(&tracker, &ps2rpc::ActivityTracker::stateChanged, &factory, &ps2rpc::PresenceFactory::setActivityFromGameState);
-    QObject::connect(&factory, &ps2rpc::PresenceFactory::activityChanged, &presence_handler, &ps2rpc::PresenceHandler::setActivity);
-
-    return a.exec();
+    QApplication app(argc, argv);
+    QCoreApplication::setOrganizationName("Leonhard S.");
+    QCoreApplication::setApplicationName("PS2 Rich Presence");
+    QCoreApplication::setApplicationVersion(PS2RPC_VERSION);
+    return app.exec();
 }
