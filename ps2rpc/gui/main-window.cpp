830e1ee5e39e3141684cb43428c430ef2e7c9982
Leonhard S <leonhard-sei@outlook.com>
2022-02-12 13:25:54 +0100

    Make GUI strings translatable

--- ps2rpc/gui/main-window.cpp
+++ ps2rpc/gui/main-window.cpp
@@ -43,7 +43,7 @@ namespace ps2rpc
     {
         if (events_per_second < 0.0)
         {
-            event_frequency_->setText("Unknown");
+            event_frequency_->setText(tr("Unknown"));
         }
         else
         {
@@ -55,17 +55,17 @@ namespace ps2rpc
     {
         if (latency_ms < 0)
         {
-            latency_->setText("Unknown");
+            latency_->setText(tr("Unknown"));
         }
         else
         {
-            latency_->setText("%1 ms".arg(latency_ms));
+            latency_->setText(tr("%1 ms").arg(latency_ms));
         }
     }
 
     void MainWindow::setLastPayload(bool never)
     {
-        payload_ago_->setText("Never");
+        payload_ago_->setText(tr("Never"));
     }
 
     void MainWindow::setLastPayload(QDateTime timestamp)
@@ -75,7 +75,7 @@ namespace ps2rpc
 
     void MainWindow::setLastPresence(bool never)
     {
-        presence_ago_->setText("Never");
+        presence_ago_->setText(tr("Never"));
     }
 
     void MainWindow::setLastPresence(QDateTime timestamp)
@@ -85,7 +85,7 @@ namespace ps2rpc
 
     void MainWindow::setStatus(const QString &status)
     {
-        status_->setText("Status: " + status);
+        status_->setText(tr("Status: ") + status);
     }
 
     void MainWindow::setupUi()
@@ -97,12 +97,12 @@ namespace ps2rpc
 
         auto characters_layout = new QHBoxLayout();
         layout->addLayout(characters_layout);
-        auto character_label = new QLabel("Character:", this);
+        auto character_label = new QLabel(tr("Character:"), this);
         characters_layout->addWidget(character_label);
         characters_combo_box_ = new QComboBox(this);
         characters_layout->addWidget(characters_combo_box_);
         characters_combo_box_->insertSeparator(0);
-        characters_combo_box_->addItem("Manage characters…", QVariant(0));
+        characters_combo_box_->addItem(tr("Manage characters…"), QVariant(0));
         characters_layout->setStretch(0, 1);
         characters_layout->setStretch(1, 2);
 
@@ -118,29 +118,33 @@ namespace ps2rpc
 
         // Statistics group
 
-        auto statistics_group = new QGroupBox("Statistics", this);
+        auto statistics_group = new QGroupBox(tr("Statistics"), this);
         layout->addWidget(statistics_group);
         auto statistics_layout = new QGridLayout(statistics_group);
 
-        auto latency_label = new QLabel("Event stream latency:", this);
+        auto latency_label = new QLabel(
+            tr("Event stream latency") + ":", this);
         statistics_layout->addWidget(latency_label, 0, 0);
         latency_ = new QLabel("", this);
         setEventLatency(-1); // "Unknown"
         statistics_layout->addWidget(latency_, 0, 1);
 
-        auto frequency_label = new QLabel("Events per second:", this);
+        auto frequency_label = new QLabel(
+            tr("Events per second") + ":", this);
         statistics_layout->addWidget(frequency_label, 1, 0);
         event_frequency_ = new QLabel("", this);
         setEventFrequency(-1.0); // "Unknown"
         statistics_layout->addWidget(event_frequency_, 1, 1);
 
-        auto payload_ago_label = new QLabel("Last event payload:", this);
+        auto payload_ago_label = new QLabel(
+            tr("Last event payload") + ":", this);
         statistics_layout->addWidget(payload_ago_label, 2, 0);
         payload_ago_ = new QLabel("", this);
         setLastPayload(false); // "Never"
         statistics_layout->addWidget(payload_ago_, 2, 1);
 
-        auto presence_ago_label = new QLabel("Last presence update:", this);
+        auto presence_ago_label = new QLabel(
+            tr("Last presence update") + ":", this);
         statistics_layout->addWidget(presence_ago_label, 3, 0);
         presence_ago_ = new QLabel("", this);
         setLastPresence(false); // "Never"
@@ -150,18 +154,18 @@ namespace ps2rpc
 
         // Settings group
 
-        auto settings_group = new QGroupBox("Settings", this);
+        auto settings_group = new QGroupBox(tr("Settings"), this);
         layout->addWidget(settings_group);
         auto settings_layout = new QVBoxLayout(settings_group);
 
         auto_track_ = new QCheckBox(
-            "Automatically track characters on login", this);
+            tr("Automatically track characters on login"), this);
         settings_layout->addWidget(auto_track_);
 
-        start_with_windows_ = new QCheckBox("Start with Windows", this);
+        start_with_windows_ = new QCheckBox(tr("Start with Windows"), this);
         settings_layout->addWidget(start_with_windows_);
 
-        minimise_to_tray_ = new QCheckBox("Minimise to system tray", this);
+        minimise_to_tray_ = new QCheckBox(tr("Minimise to system tray"), this);
         settings_layout->addWidget(minimise_to_tray_);
 
         // Footer
@@ -178,7 +182,7 @@ namespace ps2rpc
         footer_labels_layout->addWidget(appinfo_label);
 
         auto link = getProjectLink();
-        auto title = "Project Repository";
+        auto title = tr("Project Repository");
         auto project_text = QString("[") + title + "]" + "(" + link + ")";
         auto repo_label = new QLabel(project_text, this);
         footer_labels_layout->addWidget(repo_label);
@@ -189,9 +193,9 @@ namespace ps2rpc
         auto footer_buttons_layout = new QHBoxLayout();
         layout->addLayout(footer_buttons_layout);
 
-        connect_button_ = new QPushButton("Connect", this);
+        connect_button_ = new QPushButton(tr("Connect"), this);
         footer_buttons_layout->addWidget(connect_button_);
-        minimise_button_ = new QPushButton("Minimize", this);
+        minimise_button_ = new QPushButton(tr("Minimize"), this);
         minimise_button_->setChecked(true);
         footer_buttons_layout->addWidget(minimise_button_);
     }
