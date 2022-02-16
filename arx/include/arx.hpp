41ca76ff829f27b6afda6b414a5ce67b751398f8
Leonhard S <leonhard-sei@outlook.com>
2022-02-16 16:54:36 +0100

    Replicate support module

---
+++ arx/include/arx.hpp
@@ -0,0 +1,9 @@
+// Copyright 2022 Leonhard S.
+
+#ifndef ARX_HPP
+#define ARX_HPP
+
+#include "arx/payload.hpp"
+#include "arx/support.hpp"
+
+#endif // ARX_HPP
---
+++ arx/include/arx/support.hpp
@@ -0,0 +1,52 @@
+// Copyright 2022 Leonhard S.
+
+#ifndef ARX_SUPPORT_HPP
+#define ARX_SUPPORT_HPP
+
+#include <string>
+#include <vector>
+
+namespace arx
+{
+
+    enum class SearchModifier
+    {
+        EQUAL_TO,
+        NOT_EQUAL_TO,
+        GREATER_THAN,
+        GREATER_THAN_OR_EQUAL_TO,
+        LESS_THAN,
+        LESS_THAN_OR_EQUAL_TO,
+        CONTAINS,
+        STARTS_WITH,
+    };
+
+    SearchModifier modifierFromData(const std::string &data);
+
+    std::string serialiseModifier(SearchModifier modifier);
+
+    class SearchTerm
+    {
+    public:
+        SearchTerm();
+        SearchTerm(const std::string &field, const std::string &value,
+                   SearchModifier modifier = SearchModifier::EQUAL_TO);
+
+        SearchTerm static createFromValue(const std::string &field,
+                                          const std::string &value);
+
+        std::pair<std::string, std::string> asQueryItem() const;
+        std::string serialise() const;
+
+    private:
+        std::string field_;
+        std::string value_;
+        SearchModifier modifier_;
+    };
+
+    std::string join(const std::vector<std::string> &strings,
+                     const std::string &delimiter);
+
+} // namespace arx
+
+#endif // ARX_SUPPORT_HPP
