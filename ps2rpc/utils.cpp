leonhard-s/ps2-rich-presence

commit 4bcd8512b254c76eef9cad884adc361ed02b6cb8

Move main application to new Arx version

ps2rpc/utils.cpp
@@ -2,14 +2,51 @@
 
 #include "utils.hpp"
 
-#include <QtCore/QJsonObject>
-#include <QtCore/QJsonDocument>
+#include <string>
+
 #include <QtCore/QString>
 #include <QtCore/QUrl>
 #include <QtCore/QUrlQuery>
 #include <QtNetwork/QNetworkReply>
 
 #include "arx.hpp"
+#include "ps2.hpp"
+
+namespace
+{
+
+    arx::json_string stringViaJsonKey(const arx::json_object &object,
+                                      const arx::json_string &key)
+    {
+        auto it = object.find(key);
+        if (it == object.end())
+        {
+            qWarning() << "JSON key not found:" << key.c_str() << "\n"
+                       << object.dump().c_str();
+            return "";
+        }
+        if (!it->is_string())
+        {
+            qWarning() << "JSON key is not a string:" << key.c_str() << "\n"
+                       << object.dump().c_str();
+            return "";
+        }
+        return it->get<arx::json_string>();
+    }
+
+    template <typename T>
+    T quotedIntegerViaJsonKey(const arx::json_object &object,
+                              const arx::json_string &key)
+    {
+        auto val_str = stringViaJsonKey(object, key);
+        if (val_str == "")
+        {
+            return 0;
+        }
+        return static_cast<T>(std::strtoull(val_str.c_str(), nullptr, 10));
+    }
+
+} // namespace
 
 namespace ps2rpc
 {
@@ -30,11 +67,82 @@ namespace ps2rpc
         return url;
     }
 
-    QJsonObject getJsonPayload(QNetworkReply &reply)
+    arx::json_object getJsonPayload(QNetworkReply &reply)
     {
-        auto data = static_cast<QString>(reply.readAll());
-        auto doc = QJsonDocument::fromJson(data.toUtf8());
-        return doc.object();
+        return arx::json_object::parse(reply.readAll().toStdString());
+    }
+
+    ps2::CharacterId characterIdFromJson(const arx::json_object &object)
+    {
+        auto id_str = stringViaJsonKey(object, "character_id");
+        if (id_str.empty())
+        {
+            qWarning() << "Invalid character ID" << id_str.c_str();
+            return 0;
+        }
+        return std::strtoull(id_str.c_str(), nullptr, 10);
+    }
+
+    std::string characterNameFromJson(const arx::json_object &object)
+    {
+        auto it = object.find("name");
+        if (it == object.end())
+        {
+            qWarning() << "JSON key not found: name\n"
+                       << object.dump().c_str();
+            return "N/A";
+        }
+        if (!it->is_object())
+        {
+            qWarning() << "JSON key is not an object: name\n"
+                       << object.dump().c_str();
+            return "N/A";
+        }
+        return stringViaJsonKey(*it, "first");
+    }
+
+    ps2::Faction factionFromJson(const arx::json_object &object)
+    {
+        auto id = quotedIntegerViaJsonKey<ps2::FactionId>(object, "faction_id");
+        ps2::Faction faction = ps2::Faction::NS;
+        if (!ps2::faction_from_faction_id(id, faction))
+        {
+            qWarning() << "Invalid faction_id" << id;
+        }
+        return faction;
+    }
+
+    ps2::Class classFromJsonLoadout(const arx::json_object &payload)
+    {
+        auto id = quotedIntegerViaJsonKey<ps2::LoadoutId>(payload, "loadout_id");
+        ps2::Class class_ = ps2::Class::LightAssault;
+        if (!ps2::class_from_loadout_id(id, class_))
+        {
+            qWarning() << "Invalid loadout_id" << id;
+        }
+        return class_;
+    }
+
+    ps2::Class classFromJsonProfile(const arx::json_object &payload)
+    {
+        auto id = quotedIntegerViaJsonKey<ps2::ProfileId>(payload, "profile_id");
+        ps2::Class class_ = ps2::Class::LightAssault;
+        if (!ps2::class_from_profile_id(id, class_))
+        {
+            qWarning() << "Invalid profile_id" << id;
+        }
+        return class_;
+    }
+
+    ps2::Server serverFromJson(const arx::json_object &object)
+    {
+        auto id = quotedIntegerViaJsonKey<ps2::WorldId>(object, "world");
+        ps2::Server server = ps2::Server::Connery;
+        if (!ps2::server_from_world_id(id, server))
+        {
+            qWarning() << "Invalid world" << id;
+        }
+        return server;
     }
 
 } // namespace ps2rpc

ps2rpc/game/character-info.cpp
@@ -169,9 +169,10 @@ namespace ps2rpc
         return QNetworkRequest(url);
     }
 
-    void CharacterInfo::handleCharacterInfoPayload(const QJsonObject &payload)
+    void CharacterInfo::handleCharacterInfoPayload(
+        const arx::json_object &payload)
     {
-        if (!arx::isPayloadValid("character", payload))
+        if (!arx::validatePayload("character", payload))
         {
             qWarning() << "CharacterInfo::handleCharacterInfoPayload(): "
                           "Invalid JSON payload";
@@ -180,29 +181,26 @@ namespace ps2rpc
         // Get character object
         auto data = arx::payloadResultAsObject("character", payload);
         // Get new fields from payload
-        auto new_id = data["character_id"].toString().toLongLong();
-        auto new_name = data["name"].toObject()["first"].toString();
-        auto faction_id = data["faction_id"].toString().toInt();
-        auto profile_id = data["profile_id"].toString().toInt();
-        auto world_id = data["world"].toObject()["world_id"].toString().toInt();
-        // Convert ps2-specific IDs
-        ps2::Faction new_faction;
-        if (ps2::faction_from_faction_id(faction_id, new_faction))
+        auto new_id = characterIdFromJson(data);
+        auto new_name = QString::fromStdString(characterNameFromJson(data));
+        auto new_faction = factionFromJson(data);
+        auto new_class = classFromJsonProfile(data);
+        // Resolve characters_world join
+        auto new_server = ps2::Server::Connery;
+        auto world_data = data.find("world");
+        if (world_data == data.end())
         {
-            qWarning() << "Failed to convert faction ID" << faction_id;
-            return;
+            qWarning() << "CharacterInfo::handleCharacterInfoPayload():"
+                       << "No world join data in payload";
         }
-        ps2::Class new_class;
-        if (ps2::class_from_profile_id(profile_id, new_class))
+        else if (!world_data->is_object())
         {
-            qWarning() << "Failed to get class from profile ID" << profile_id;
-            return;
+            qWarning() << "CharacterInfo::handleCharacterInfoPayload():"
+                       << "World join data is not an object";
         }
-        ps2::Server new_server;
-        if (ps2::server_from_world_id(world_id, new_server))
+        else
         {
-            qWarning() << "Failed to get server from world ID" << world_id;
-            return;
+            new_server = serverFromJson(*world_data);
         }
         // Update fields
         updateFieldsIfChanged(

ps2rpc/game/character-info.hpp
@@ -4,7 +4,6 @@
 #define PS2RPC_GAME_CHARACTER_INFO_HPP
 
 #include <QtCore/QDebug>
-#include <QtCore/QJsonObject>
 #include <QtCore/QMetaType>
 #include <QtCore/QObject>
 #include <QtCore/QScopedPointer>
@@ -12,6 +11,7 @@
 #include <QtNetwork/QNetworkAccessManager>
 #include <QtNetwork/QNetworkRequest>
 
+#include "arx.hpp"
 #include "ps2.hpp"
 
 namespace ps2rpc
@@ -59,6 +59,8 @@ namespace ps2rpc
         ps2::Faction getFaction() const;
         ps2::Class getClass() const;
         ps2::Server getServer() const;
+        
+        void handleCharacterInfoPayload(const arx::json_object &payload);
 
     Q_SIGNALS:
         void infoChanged();
@@ -71,7 +73,6 @@ namespace ps2rpc
 
     private:
         QNetworkRequest getCharacterInfoRequest();
-        void handleCharacterInfoPayload(const QJsonObject &payload);
         void updateFieldsIfChanged(ps2::CharacterId id,
                                    const QString &name,
                                    ps2::Faction faction,

ps2rpc/gui/character-manager.cpp
@@ -159,9 +159,9 @@ namespace ps2rpc
             return;
         }
         // Validate payload
-        const QString collection = "character";
+        const std::string collection = "character";
         auto payload = getJsonPayload(*reply);
-        if (!arx::isPayloadValid(collection, payload))
+        if (!arx::validatePayload(collection, payload))
         {
             QMessageBox::critical(this,
                                   tr("Character Manager"),
@@ -177,9 +177,11 @@ namespace ps2rpc
                                   QMessageBox::Ok);
             return;
         }
-        auto result = arx::payloadResultAsObject(collection, payload);
-        // Parse character data
-        auto info = parseCharacterPayload(result);
+        // HACK: Parse character data
+        CharacterInfo temp;
+        temp.handleCharacterInfoPayload(payload);
+        CharacterData info{temp.getId(), temp.getName(), temp.getFaction(),
+                           temp.getClass(), temp.getServer()};
         // Create character entry
         auto item = new QListWidgetItem(info.name);
         item->setData(Qt::UserRole, QVariant::fromValue(info));
@@ -202,28 +204,87 @@ namespace ps2rpc
         return qUrlFromArxQuery(query);
     }
 
-    CharacterData CharacterManager::parseCharacterPayload(const QJsonObject &payload)
+    CharacterData CharacterManager::parseCharacterPayload(
+        const arx::json_object &payload)
     {
-        // Extract character data from payload
-        auto name = payload["name"].toObject()["first"].toString();
-        ps2::CharacterId id = payload["character_id"].toString().toULongLong();
-        ps2::FactionId faction_id = payload["faction_id"].toString().toInt();
-        ps2::ProfileId profile_id = payload["profile_id"].toString().toInt();
-        auto world_data = payload["world"].toObject();
-        ps2::WorldId world_id = world_data["world_id"].toString().toInt();
+        // Set default/fallback values
+        QString name = "N/A";
+        ps2::CharacterId id = 0;
+        ps2::FactionId faction_id = 0;
+        ps2::ProfileId profile_id = 0;
+        ps2::WorldId world_id = 0;
+        ps2::Faction faction = ps2::Faction::NS;
+        ps2::Class class_ = ps2::Class::LightAssault;
+        ps2::Server server = ps2::Server::Connery;
+        // Get name object
+        auto name_obj = payload["name"];
+        if (name_obj.is_object())
+        {
+            // Get name.first value
+            auto name_val = name_obj["first"];
+            if (name_val.is_string())
+            {
+                name = QString::fromStdString(name_val.get<std::string>());
+            }
+            else
+            {
+                qWarning() << "Invalid type: \"name.first\" must be a string";
+            }
+        }
+        else
+        {
+            qWarning() << "Invalid type: key \"name\" must be an object";
+        }
+        // Get character ID
+        auto id_val = payload["character_id"];
+        if (id_val.is_string())
+        {
+            id = std::stoull(id_val.get<std::string>());
+        }
+        else
+        {
+            qWarning() << "Invalid type: \"character_id\" must be a string";
+        }
+        // Get faction
+        auto faction_val = payload["faction_id"];
+        if (faction_val.is_number_integer())
+        {
+            faction_id = faction_val.get<ps2::FactionId>();
+        }
+        else
+        {
+            qWarning() << "Invalid type: \"faction_id\" must be a number";
+        }
+        // Get profile
+        auto profile_val = payload["profile_id"];
+        if (profile_val.is_number_integer())
+        {
+            profile_id = profile_val.get<ps2::ProfileId>();
+        }
+        else
+        {
+            qWarning() << "Invalid type: \"profile_id\" must be a number";
+        }
+        // Get world
+        auto world_val = payload["world_id"];
+        if (world_val.is_number_integer())
+        {
+            world_id = world_val.get<ps2::WorldId>();
+        }
+        else
+        {
+            qWarning() << "Invalid type: \"world_id\" must be a number";
+        }
         // Convert IDs to enum values
-        auto faction = ps2::Faction::NS;
         if (ps2::faction_from_faction_id(faction_id, faction))
         {
             qWarning() << "Unable to convert faction ID:" << faction_id;
         }
-        auto class_ = ps2::Class::LightAssault;
         if (ps2::class_from_profile_id(profile_id, class_))
         {
             qWarning() << "Unable to create class from profile ID:"
                        << profile_id;
         }
-        auto server = ps2::Server::Connery;
         if (ps2::server_from_world_id(world_id, server))
         {
             qWarning() << "Unable to create server from world ID:" << world_id;

ps2rpc/gui/character-manager.hpp
@@ -13,6 +13,8 @@
 #include <QtWidgets/QListWidget>
 #include <QtWidgets/QPushButton>
 
+#include "arx.hpp"
+
 #include "game/character-info.hpp"
 
 namespace ps2rpc
@@ -39,7 +41,7 @@ namespace ps2rpc
 
     private:
         QUrl getCharacterInfoUrl(const QString &character) const;
-        CharacterData parseCharacterPayload(const QJsonObject &payload);
+        CharacterData parseCharacterPayload(const arx::json_object &payload);
         QDialog *createCharacterNameInputDialog();
         void setupUi();
 

ps2rpc/tracker.cpp
@@ -3,6 +3,7 @@
 #include "tracker.hpp"
 
 #include <QtCore/QJsonArray>
+#include <QtCore/QJsonDocument>
 #include <QtCore/QJsonObject>
 #include <QtCore/QObject>
 #include <QtCore/QScopedPointer>
@@ -40,8 +41,12 @@ namespace ps2rpc
         return character_;
     }
 
-    void ActivityTracker::onPayloadReceived(const QString &event_name, const QJsonObject &payload)
+    void ActivityTracker::onPayloadReceived(const QString &event_name,
+                                            const arx::json_object &arx_payload)
     {
+        // HACK: Replace payload with Qt JSON version
+        auto doc = QJsonDocument::fromJson(arx_payload.dump().c_str());
+        auto payload = doc.object();
         emit payloadReceived(event_name, payload);
         // Character ID
         bool are_we_the_baddies = payload["attacker_character_id"]

ps2rpc/tracker.hpp
@@ -8,6 +8,7 @@
 #include <QtCore/QScopedPointer>
 #include <QtCore/QString>
 
+#include "arx.hpp"
 #include "arx/ess.hpp"
 #include "ps2.hpp"
 
@@ -35,7 +36,7 @@ namespace ps2rpc
 
     private Q_SLOTS:
         void onPayloadReceived(const QString &event_name,
-                               const QJsonObject &payload);
+                               const arx::json_object &payload);
 
     private:
         arx::Subscription generateSubscription() const;

ps2rpc/utils.hpp
@@ -3,18 +3,32 @@
 #ifndef PS2RPC_UTILS_HPP
 #define PS2RPC_UTILS_HPP
 
-#include <QtCore/QJsonObject>
+#include <string>
+
 #include <QtCore/QUrl>
 #include <QtNetwork/QNetworkReply>
 
 #include "arx.hpp"
+#include "ps2.hpp"
 
 namespace ps2rpc
 {
 
     QUrl qUrlFromArxQuery(const arx::Query &query);
 
-    QJsonObject getJsonPayload(QNetworkReply &reply);
+    arx::json_object getJsonPayload(QNetworkReply &reply);
+
+    ps2::CharacterId characterIdFromJson(const arx::json_object &object);
+
+    std::string characterNameFromJson(const arx::json_object &object);
+
+    ps2::Faction factionFromJson(const arx::json_object &object);
+
+    ps2::Class classFromJsonLoadout(const arx::json_object &object);
+
+    ps2::Class classFromJsonProfile(const arx::json_object &object);
+
+    ps2::Server serverFromJson(const arx::json_object &object);
 
 } // namespace ps2rpc
 

commit 06fe7d4003daa1aea291f5a8400eece802fe57fd

Fix all characters being on Connery
This fixes a typo in the "world" join payload key introduced in 4bcd8512.

ps2rpc/utils.cpp
@@ -136,7 +136,7 @@ namespace ps2rpc
 
     ps2::Server serverFromJson(const arx::json_t &object)
     {
-        auto id = quotedIntegerViaJsonKey<arx::world_id_t>(object, "world");
+        auto id = quotedIntegerViaJsonKey<arx::world_id_t>(object, "world_id");
         ps2::Server server = ps2::Server::Connery;
         if (ps2::server_from_world_id(id, server))
         {